d1c6060aa2ef3b53d9353a15f24371e7edb758f5
zheen <suseyizheen@gmail.com>
2025-02-02 14:48:43 -0800

    Initial Commit

---
+++ SetCardGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SetCardGame
+//
+//  Created by Zheen Suseyi on 2/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SetCardGame/SetCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardGameApp.swift
+//  SetCardGame
+//
+//  Created by Zheen Suseyi on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SetCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
